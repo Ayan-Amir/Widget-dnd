d8dfe91d7e0a536fbc12423da27dd823fc48fbf1
Ayyan <ayyanamir6@gmail.com>
2024-03-19 12:34:51 +0500

    update widget dnd

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,104 @@
-import { useState } from 'react'
-import './App.css'
-import ReactGridLayout from 'react-grid-layout';
-import 'react-grid-layout/css/styles.css';
-import 'react-resizable/css/styles.css';
+import { useState } from "react";
+import "./App.css";
+import ReactGridLayout from "react-grid-layout";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
+import { Bar } from "react-chartjs-2";
+import { CategoryScale } from "chart.js";
+Chart.register(CategoryScale);
+import Chart from "chart.js/auto";
+
+const options = {
+	responsive: true,
+	maintainAspectRatio: false,
+	plugins: {
+		legend: {
+			position: "top",
+		},
+		title: {
+			display: true,
+			text: "Chart.js Bar Chart",
+		},
+	},
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+const data = {
+	labels,
+	datasets: [
+		{
+			label: "Dataset 1",
+			barPercentage: 0.5,
+			barThickness: 6,
+			maxBarThickness: 8,
+			minBarLength: 2,
+			data: [12, 19, 3, 5, 2, 3],
+			backgroundColor: "rgba(255, 99, 132, 0.5)",
+		},
+		{
+			label: "Dataset 2",
+			data: [12, 19, 3, 5, 2, 3],
+			backgroundColor: "rgba(53, 162, 235, 0.5)",
+		},
+	],
+};
 
 function App() {
-  const [widgets, setWidgets] = useState([]);
+	const [widgets, setWidgets] = useState([]);
+	const [widgetCounter, setWidgetCounter] = useState(0);
 
-  const handleOnDrag = (e, widgetType) => {
-    e.dataTransfer.setData('widgetType', widgetType)
-  }
+	const handleOnDrag = (e, widgetType) => e.dataTransfer.setData("widgetType", widgetType);
 
-  const handleOnDrop = (e) => {
-    const widgetType =  e.dataTransfer.getData('widgetType');
-    setWidgets([...widgets, widgetType])
-  }
+	const handleOnDrop = e => {
+		const widgetType = e.dataTransfer.getData("widgetType");
 
-  const handleDragOver = (e) => {
-    e.preventDefault();
-  }
+		setWidgets([
+			...widgets,
+			{ i: `${widgetType}-${widgetCounter}`, x: (widgets?.length * 3) % 12, y: Infinity, w: 3, h: 3 },
+		]);
+		setWidgetCounter(widgetCounter + 1);
+	};
 
-  const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 }
-  ];
+	const handleDragOver = e => e.preventDefault();
 
-  return (
-    <div className='App'>
-      <div className='widgets'>
-        <div className='widget' draggable onDragStart={(e) => handleOnDrag(e, 'Widget 1')}>Widget One</div>
-        <div className='widget' draggable onDragStart={(e) => handleOnDrag(e, 'Widget 2')}>Widget Two</div>
-        <div className='widget' draggable onDragStart={(e) => handleOnDrag(e, 'Widget 3')}>Widget Three</div>
-      </div>
-      <div className='widget-container' onDrop={handleOnDrop} onDragOver={handleDragOver}>
-      <ReactGridLayout
-        className="layout custom-layout"
-        layout={layout}
-        cols={12}
-        rowHeight={30}
-        width={820}
-      >
-        {
-          widgets.map((widget, index) => (
-            <div key={index} className='dropped-widget'>
-              {widget}
-            </div>
-          ))
-        }
-        </ReactGridLayout>
-      </div>
-    </div>
-  )
+	let widgetsList = ["widget-1", "widget-2", "widget-3", "widget-4", "widget-5", "widget-6"];
+
+	console.log(widgets);
+
+	return (
+		<div className='App'>
+			<div className='widgets'>
+				{widgetsList?.map((item, i) => (
+					<div key={i} id={item} className='widget' draggable onDragStart={e => handleOnDrag(e, `${item}`)}>
+						{item}
+					</div>
+				))}
+			</div>
+			<div className='widget-container' onDrop={handleOnDrop} onDragOver={handleDragOver}>
+				<ReactGridLayout
+					className='layout custom-layout'
+					layout={widgets}
+					cols={12}
+					autoSize={true}
+					isResizable={true}
+					rowHeight={30}
+					width={820}
+					onLayoutChange={l => setWidgets(l)}
+				>
+					{widgets.map(widget => (
+						<div key={widget?.i} className='dropped-widget'>
+							{widget?.i.includes("widget-1") && (
+								<div style={{ height: "100%", width: "100%" }}>
+									<Bar options={options} data={data} />
+								</div>
+							)}
+						</div>
+					))}
+				</ReactGridLayout>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
